6dc10f43b7b2333e56e4876824e341b2cab34e61
alexsyrom <alexsyrom@yandex.ru>
2014-05-05 15:21:01 +0400

    new settings

--- dgap_voting/settings.py
+++ dgap_voting/settings.py
@@ -55,19 +55,25 @@ WSGI_APPLICATION = 'dgap_voting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# SECURITY WARNING: don't run with this passwords in production!
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dgap_voting_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres2014',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
