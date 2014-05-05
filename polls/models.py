702cef2f0eb102867453a62cb680f6677a9885c9
alexsyrom <alexsyrom@yandex.ru>
2014-05-05 15:32:10 +0400

    polls

---
+++ polls/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Poll(models.Model):
+    name = models.CharField('Название опроса', max_length=50)
+    question = models.CharField('Вопрос', max_length=200)
+    begin_date = models.DateTimeField('Начало голосования')
+    end_date = models.DateTimeField('Конец голосования')
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
