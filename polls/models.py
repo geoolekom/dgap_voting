80076fd8d8c221129f3c339a37e01b3380406472
alexsyrom <alexsyrom@yandex.ru>
2014-05-23 22:52:23 +0400

    орфография

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.utils import timezone
 
 #TODO необходимые методы в моделях
 #TODO метод approve_user и модели для базы поселения
