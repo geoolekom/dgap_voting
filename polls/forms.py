19a9f8cd66e48b34264046e388ce27ee111cac96
alexsyrom <alexsyrom@yandex.ru>
2014-05-24 08:31:31 +0400

    no password

--- polls/forms.py
+++ polls/forms.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name')
         widgets = {
             'username': forms.TextInput(attrs={'readonly': True}),
-            'password': forms.PasswordInput(),
         }
 
 class UserProfileForm(forms.ModelForm):
