2bf3fabd41045f8620888851d88e692cf9cd67e8
alexsyrom <alexsyrom@yandex.ru>
2014-08-26 23:07:45 +0400

    how can i forgive the template

--- dgap_voting/urls.py
+++ dgap_voting/urls.py
@@ -3,8 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return "/polls/profile"
+
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 )
