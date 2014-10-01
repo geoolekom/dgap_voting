246a98691897d13a87aed9f6064af5dd141b2fa0
alexsyrom <alexsyrom@yandex.ru>
2014-10-01 23:42:16 +0400

    непонятка с папками для static -- 2

--- dgap_voting/settings.py
+++ dgap_voting/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/polls/'
