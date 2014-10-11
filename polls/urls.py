fcc2f1584e5e3206866f3b89075ff596d7aa897c
alexsyrom <alexsyrom@yandex.ru>
2014-10-11 18:21:13 +0400

    теперь используем пути по умолчанию для django.auth

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.views import login, logout, password_change, password_change_done
 
 from polls import views
 
@@ -14,10 +13,6 @@ urlpatterns = patterns('',
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
     # ex:
     url(r'^done/$', views.done, name='done'),
-    url(r'^login/$', login, {'template_name': 'polls/login.html'}, name = 'login'),
-    url(r'^logout/$', logout, {'template_name': 'polls/logout.html'}, name = 'logout'),
-    url(r'^password_change/$', password_change, {'post_change_redirect': 'polls:password_change_done','template_name': 'polls/password_change.html'}, name = 'password_change'),
-    url(r'^password_change_done/$', password_change_done, { 'template_name': 'polls/password_change_done.html'}, name = 'password_change_done'),
 #    url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.profile_view, name='profile_view'),
 )
