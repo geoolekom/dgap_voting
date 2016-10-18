15f9c9e404ed64ea7e0e201609e0a90c8b8ac8b7
Pavel <pavel.semenenko@phystech.edu>
2016-10-18 13:59:18 +0300

    Added collapse.js

--- polls/static/polls/collapse.js
+++ polls/static/polls/collapse.js
@@ -1,5 +1,5 @@
 $( ".panel-heading" ).click(function () {
-    var panel_body = this.parent().find(".panel-body");
+    var panel_body = $(this).parent().find(".panel-body");
     if (panel_body.is(":visible")) {
         panel_body.hide();
         alert("LOL");
