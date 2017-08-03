3df57244af0790c5238448a75781b32833915948
pavel <pavel.semenenko@Phystech.edu>
2017-08-03 18:15:28 +0300

    Added fin_aid to nav menu

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog import views
 urlpatterns = [
     url(r'^$', views.ArticleList.as_view(), name='index'),
     url(r'^$', views.ArticleList.as_view(), name='article_list'),
-    url(r'^post-(?P<pk>\d+)$', views.ArticleDetail.as_view(), name='article_detail'),
+    url(r'^post/(?P<pk>\d+)$', views.ArticleDetail.as_view(), name='article_detail'),
     url(r'^new_post$', views.ArticleCreate.as_view(), name="new_post"),
 
 ]
