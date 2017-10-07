4e6d53ca8170021570947ae5f414c5a64704e4e6
FUPM <pavel.semenenko@phystech.edu>
2017-10-07 14:21:37 +0300

    * Added Yandex.Metrika * Total sum of WAITING aid requests is now displayed in admin

--- fin_aid/templatetags/fin_aid_extras.py
+++ fin_aid/templatetags/fin_aid_extras.py
@@ -1,6 +1,7 @@
 from django import template
+from django.db.models import Sum
 import os
-from fin_aid.models import MonthlyData
+from fin_aid.models import MonthlyData, AidRequest
 
 
 register = template.Library()
@@ -8,12 +9,13 @@ register = template.Library()
 
 @register.simple_tag
 def month_info():
-    text = "Сейчас: {}, Лимит: {:.0f}, Использовано: {:.0f}, Профицит: {:.0f}"
+    text = "Сейчас: {}, Лимит: {:.0f}, Использовано: {:.0f}, Профицит: {:.0f}, Рассмаатривается: {:.0f}"
     current = MonthlyData.current()
     used = current.sum_used
+    waiting = AidRequest.objects.filter(status=AidRequest.WAITING).aggregate(Sum('req_sum'))['req_sum__sum']
     if not used:
         used = 0
-    return text.format(current.get_month_display(), current.limit, used, current.limit - used)
+    return text.format(current.get_month_display(), current.limit, used, current.limit - used, waiting)
 month_info.allow_tags = True
 
 
