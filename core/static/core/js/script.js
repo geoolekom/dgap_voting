d1f8404f9e202639c26dda8de861fc537f19e5fa
FUPM <pavel.semenenko@phystech.edu>
2017-10-11 20:45:02 +0300

    Moved scripts from `core/base.html` to `core/js/script.js`

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -3,3 +3,31 @@ jQuery(document).ready(function($) {
         window.location = $(this).data("href");
     });
 });
+
+function serverTime() {
+    var time = null;
+    $.ajax({url: "{% url 'servertime:server_time' %}",
+        async: false, dataType: 'text',
+        success: function(text) {
+            time = new Date(text);
+        }, error: function(http, message, exc) {
+            time = new Date();
+    }});
+    return time;
+};
+function serverDate() {
+    var time = null;
+    $.ajax({url: "{% url 'servertime:server_date' %}",
+        async: false, dataType: 'text',
+        success: function(text) {
+            time = new Date(text);
+        }, error: function(http, message, exc) {
+            time = new Date();
+    }});
+    return time;
+};
+
+$(function () {
+        var startDay = serverDate();
+        $('#defaultTime').countdown({since: startDay, compact: true, format: 'HMS', description: '', serverSync: serverTime});
+});
