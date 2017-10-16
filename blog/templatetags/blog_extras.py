cfc740db7f426abea8dcd6c58eeb8b314ebcb103
FUPM <pavel.semenenko@phystech.edu>
2017-10-16 14:53:31 +0300

    Blog posts now can be embedded using templatetags

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,34 @@
+from django import template
+
+from blog.models import Article
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def article_content(slug):
+    post = Article.objects.get(slug=slug)
+    return post.content
+article_content.allow_tags = True
+
+
+@register.simple_tag
+def article_title(slug):
+    post = Article.objects.get(slug=slug)
+    return post.title
+article_title.allow_tags = True
+
+
+@register.simple_tag
+def get_article(slug):
+    return Article.objects.get(slug=slug)
+
+
+# If header_link => title is a link to post detailer view
+@register.inclusion_tag("blog/article_panel.html")
+def article_panel(slug, header_link=False, show_creation_time=False):
+    post = Article.objects.get(slug=slug)
+    return {'article': post,
+            'header_link': header_link,
+            'show_creation_time': show_creation_time}
