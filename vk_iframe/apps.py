c811e939a5412cc9f376b8612e46cc4490312316
FUPM <pavel.semenenko@phystech.edu>
2017-12-05 22:19:44 +0300

    Changed Widget generating JS

--- vk_iframe/apps.py
+++ vk_iframe/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class VkIframeConfig(AppConfig):
     name = 'vk_iframe'
+
--- vk_iframe/static/vk_iframe/vk.js
+++ vk_iframe/static/vk_iframe/vk.js
@@ -1,10 +1,48 @@
-VK.init(function() {
-    // if (VK.param('viever_type' == 4)) { // TODO check if user is admin
-    if (1) {
-        VK.addCallback('onAppWidgetPreviewFail', function f(param) {alert(param);});
-        VK.callMethod('showGroupSettingsBox', 64);
-        VK.callMethod('showAppWidgetPreviewBox', 'compact_list', 'return {"title": "Онлайн-сервисы Сената","rows": [{"title": "Матпомощь","title_url": "https://vk.com/app6216528_-117151095/#aid","button": "Подать заявление","button_url": "https://vk.com/app6216528_-117151095/#aid","text": "Крч все за матпомощью, будете в золоте"}, {"title": "Обращения в Сенат","title_url": "https://vk.com/app6216528_-117151095/#senate","button": "Подать обращение","button_url": "https://vk.com/app6216528_-117151095/#senate", "text": "Прекрасный шанс пообщаться с Кирушей"},{"title": "Бронироание клуба","title_url": "https://vk.com/app6216528_-117151095/#club","button": "Открыть расписание","button_url": "https://vk.com/app6216528_-117151095/#club","text": "Для тех, кто хотел открыть клуб просмтора аниме"},] } ;');
+VK.init(
+    function() {
+        if (VK.param('viever_type') == 4) { // TODO check if user is admin
+            alert("ADMIN!!!")
+        }
+        if (1) {
+            VK.addCallback('onAppWidgetPreviewFail', function f(param) {alert(param);});
+            VK.callMethod('showGroupSettingsBox', 64);
+            widget = getWidget();
+            VK.callMethod('showAppWidgetPreviewBox', widget["type"], 'return widget["content]"');
+        }
+    },
+    function() {
+        alert("VK API initialization failed")
+    },
+    '5.69'
+);
+
+function getWidget() {
+    return {
+        content: {
+            title: "Онлайн-сервисы Сената",
+            rows: [
+                {
+                    title: "Матпомощь",
+                    title_url: "https://vk.com/app6216528_-117151095/#aid",
+                    button: "Подать заявление",
+                    button_url: "https://vk.com/app6216528_-117151095/#aid",
+                    text: "Крч все за матпомощью, будете в золоте"
+                },
+                {
+                    title: "Обращения в Сенат",
+                    title_url: "https://vk.com/app6216528_-117151095/#senate",
+                    button: "Подать обращение",
+                    button_url: "https://vk.com/app6216528_-117151095/#senate",
+                    text: "Прекрасный шанс пообщаться с Кирушей"
+                },
+                {
+                    title: "Бронироание клуба",
+                    title_url: "https://vk.com/app6216528_-117151095/#club",
+                    button: "Открыть расписание",
+                    button_url: "https://vk.com/app6216528_-117151095/#club",
+                    text: "Для тех, кто хотел открыть клуб просмтора аниме"
+                }]
+        },
+        type: 'compact_list'
     }
-}, function() {
-    alert("VK API initialization failed")
-}, '5.69');
+}
